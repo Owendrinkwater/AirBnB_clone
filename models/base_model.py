1034f8b1669be8f191140f5d63765a6a9aa365b1
Owendrinkwater <owenowade@gmail.com>
2025-06-23 04:21:29 -0700

    Allow BaseModel to recreate instances from dictionary using kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,20 @@ class BaseModel:
     BaseModel defines all common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new instance of BaseModel."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of an instance."""
