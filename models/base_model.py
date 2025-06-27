c8bec0cd1aac1184c72586e59f178b8fe9711ce0
Owendrinkwater <owenowade@gmail.com>
2025-06-27 03:33:49 +0300

    Add FileStorage for object persistence and integrate with BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -25,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of an instance."""
@@ -33,6 +35,7 @@ class BaseModel:
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# models/engine/file_storage.py
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Serializes and deserializes instances to/from a JSON file"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the to the JSON file."""
+        obj_dict = {
+             key: obj.to_dict() for key, obj in self.__objects.items()
+        }
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserialize the JSON file to __objects, if it exists."""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    if value["__class__"] == "BaseModel":
+                        self.__objects[key] = BaseModel(**value)
